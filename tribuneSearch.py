05b540d4e7a7b50d2ba3e9a68cdb36788015658a
codenit <b.rohit751@gmail.com>
2018-03-07 00:30:28 +0530

    commit

--- tribuneSearch.py
+++ tribuneSearch.py
@@ -4,32 +4,38 @@ import time
 
 
 def autoTribune():
+    '''
+    Function to scrap headlines and bulletins from the website.
+    '''
 
     baseUrl = "http://www.tribuneindia.com/"
 
+    # Initiates Chrome
     drivers = webdriver.Chrome()
     drivers.maximize_window()
     drivers.get(baseUrl)
-    drivers.implicitly_wait(10)
+    drivers.implicitly_wait(6)
 
     theDate = drivers.find_element(By.ID, "ctl00_header1_timespan").text
     print('News for: ','\033[4m',theDate,'\033[0m','\n')
 
+    # getting flash news from ticker.
     flashNews = drivers.find_elements(By.XPATH,"//div[@id='ctl00_topmenubar1_newsflash']//li")
     for n in flashNews:
         print("News Flash: ", n.text,'\n')
         break
 
+    time.sleep(3)
     headlines = drivers.find_element(By.XPATH,"//div[@id='ctl00_ContentPlaceHolder1_toplead_news1_dvtopleadmain']//a" ).text
     synopsis = drivers.find_element(By.ID, "synop").text
     print("Today's main Headline is: ",'\n','\t','\033[1m',headlines,'\033[0m','\n')
     print("Synopsis: ",'\n','\t', '\033[36m',synopsis,'\033[0m','\n')
 
     try:
-        response = input("Want to read full story?(y/n) ").lower(),'\n'
+        response = input("Want to read full story?(y/n) ").lower()
 
         if response == 'y':
-            drivers.find_element(By.ID, "plusicon").click()
+            drivers.find_element(By.XPATH, "//span[@id='plusicon']").click()
 
             fullArticle = drivers.find_element(By.ID, "fullart").text
             print('\033[96m',fullArticle,'\033[0m','\n')
@@ -49,5 +55,7 @@ def autoTribune():
                 print('\033[1m',b.text,'\033[0m','\n', '\033[36m', s.text,'\033[0m','\n')
     except:
         pass
+
+    drivers.close()
 
 autoTribune()
